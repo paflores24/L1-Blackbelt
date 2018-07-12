e1f31dd152796df5d342a710cfd01585e6daf563
unknown <Administrator@paulflores.trendsnet.local>
2018-07-12 14:13:56 +0800

    Programmability Black Belt Level 1 Assignment #1 Submitted by Paul Ryan Flores <paul.flores@trendsnet.com.ph>

---
+++ paflores_assign1.py
@@ -0,0 +1,7 @@
+
+Sessions_Attended = {'sessions' : '1011,2344,3222,44322,555'}
+a = Sessions_Attended['sessions'] 
+a = a.split(",")
+print("Session I attended are 1011,2344,3222,44322,555")
+print("I have attended : ", len(a), "sessions")
+
